12f590765e442210a13d7a803f937ca7f29107a3
Kento Oki <hrn832@protonmail.com>
2022-05-15 06:11:25 +0900

    WIP: implementing session class

--- src/main.js
+++ src/main.js
@@ -18,3 +18,96 @@ const ERRORS = {
     SUBMISSION_CREATE_FAILED: 'A submission creation failed',
     SUBMISSION_PRODUCT_CREATE_FAILED: 'A submission product creation failed',
 }
+
+const Session = class {
+    constructor(tenantId, clientId, clientSecret) {
+        this.#tenantId = tenantId
+        this.#clientId = clientId
+        this.#clientSecret = clientSecret
+    }
+
+    async init() {
+        var payload = new URLSearchParams();
+        payload.append('grant_type', 'client_credentials');
+        payload.append('client_id', this.#clientId);
+        payload.append('client_secret', this.#clientSecret);
+        payload.append('resource', 'https://manage.devcenter.microsoft.com');
+
+        await require('axios').create({
+            baseURL: `https://login.microsoftonline.com/${this.#tenantId
+                }/oauth2/token`,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            responseType: 'json'
+        })
+            .post('/', payload)
+            .then((res) => {
+                this.#tokenType = res.token_type
+                this.#token = res.access_token
+                this.#auth = `${this.#tokenType} ${this.#token}`
+
+                this.#axios = require('axios').create({
+                    baseURL: 'https://manage.devcenter.microsoft.com',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': this.#auth,
+                    },
+                    responseType: 'json'
+                });
+
+                core.debug('authentication succeeded')
+            })
+            .catch((err) => {
+                core.setFailed(`${ERRORS.INVALID_CREDENTIALS}}: ${err}`);
+            })
+    }
+
+    async newProduct(productName) {
+        payload = {
+            productName: productName,
+            testHarness: 'attestation',
+            announcementDate: datetime.datetime.now().isoformat('T', 'seconds'),
+            deviceMetadataIds: [],
+            firmwareVersion: '980',
+            deviceType: 'internalExternal',
+            isTestSign: False,
+            isFlightSign: False,
+            marketingNames: [],
+            selectedProductTypes: {},
+            requestedSignatures: [
+                'WINDOWS_v100_TH2_FULL',
+                'WINDOWS_v100_X64_TH2_FULL',
+                'WINDOWS_v100_RS1_FULL',
+                'WINDOWS_v100_X64_RS1_FULL',
+                'WINDOWS_v100_RS2_FULL',
+                'WINDOWS_v100_X64_RS2_FULL',
+                'WINDOWS_v100_RS3_FULL',
+                'WINDOWS_v100_X64_RS3_FULL',
+                'WINDOWS_v100_ARM64_RS3_FULL',
+                'WINDOWS_v100_RS4_FULL',
+                'WINDOWS_v100_X64_RS4_FULL',
+                'WINDOWS_v100_ARM64_RS4_FULL',
+                'WINDOWS_v100_RS5_FULL',
+                'WINDOWS_v100_X64_RS5_FULL',
+                'WINDOWS_v100_ARM64_RS5_FULL',
+                'WINDOWS_v100_19H1_FULL',
+                'WINDOWS_v100_X64_19H1_FULL',
+                'WINDOWS_v100_ARM64_19H1_FULL',
+                'WINDOWS_v100_VB_FULL',
+                'WINDOWS_v100_X64_VB_FULL',
+                'WINDOWS_v100_ARM64_VB_FULL',
+                'WINDOWS_v100_X64_CO_FULL',
+                'WINDOWS_v100_ARM64_CO_FULL',
+            ],
+            additionalAttributes: {}
+        }
+    }
+
+    async newSubmission(name, type) {
+        payload = {
+            name: name,
+            type: type
+        }
+    }
+}
